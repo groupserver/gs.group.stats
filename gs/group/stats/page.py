2a3f50db24b3d55214398cb26dbe28999410186c
Richard Waid <richard@onlinegroups.net>
2012-05-15 16:53:11 +1200

    Major rework of stats for speed

--- gs/group/stats/page.py
+++ gs/group/stats/page.py
@@ -3,10 +3,197 @@ from operator import add
 from zope.component import createObject
 from zope.cachedescriptors.property import Lazy
 from gs.content.base.page import SitePage
-from queries import GroupStatsQuery
+from queries import GroupStatsQuery, MembersAtDate
+from Products.GSGroupMember.groupMembersInfo import GSGroupMembersInfo
 
 import datetime
+import calendar
+import pytz
+import copy
+
 from zope.datetime import parseDatetimetz
+
+def month_periods(interval_start, interval_end):
+    start_year = interval_start.year
+    end_year = interval_end.year
+    start_month = interval_start.month
+    end_month = interval_end.month
+
+    periods = []
+
+    for year in range(start_year, end_year+1):
+        first_month = 1
+        last_month = 12
+        if year == start_year:
+            first_month = start_month
+        elif year == end_year:
+            last_month = end_month
+
+        for month in range(first_month, last_month+1):
+            first_weekday, number_of_days = calendar.monthrange(year,month)
+            periods.append((year, month, number_of_days))
+
+    return periods
+
+def month_periods_as_datetime(month_periods):
+    periods = []
+    for year, month, last_day in month_periods:
+        periods.append((datetime.datetime(year, month, 1, tzinfo=pytz.utc),
+                        datetime.datetime(year, month, last_day, tzinfo=pytz.utc)))
+    return periods
+
+def periods_as_dict(periods):
+    pdict = {}
+    for year, month, result in periods:
+        if not pdict.has_key(year):
+            pdict[year] = {}
+        pdict[year][month] = result    
+    
+    return pdict
+
+class HistoricalGroupStats(SitePage):
+    stats_attributes = (
+               ('new_topics', u'New Topics'),
+               ('active_topics', u'Active Topics'),
+               ('posts', u'Posts'),
+               ('authors', u'Authors'),
+               ('join_delta', u'Join Delta'),
+               ('member_average', u'Average Members'),
+               ('average_dialogue_depth', u'Average Dialogue Depth'),
+               ('min_dialogue_depth', u'Min. Dialogue Depth'),
+               ('max_dialogue_depth', u'Max. Diaglogue Depth'))
+    
+    def __init__(self, context, request):
+        SitePage.__init__(self, context, request)
+         
+        self.groupInfo = createObject('groupserver.GroupInfo', self.context)
+        self.mad = MembersAtDate(self.context, self.context.zsqlalchemy)
+        self.gs = GroupStatsQuery(self.context.zsqlalchemy)
+        self.now = datetime.datetime.now(pytz.utc)
+        self.earliest_date = self.mad.earliest_member_record(self.siteInfo.id,
+                                                        self.groupInfo.id)
+        
+        self.month_periods = month_periods_as_datetime(month_periods(self.earliest_date, self.now))
+
+    def display_stats(self):
+        return self.stats_attributes
+
+    def months(self):
+        mperiods = copy.deepcopy(self.month_periods)
+        mperiods.sort(reverse=True)
+        return mperiods
+
+    def years(self):
+        start_year = self.earliest_date.year
+        end_year = self.now.year
+        start_month = self.earliest_date.month
+        end_month = self.now.month
+        cal = []
+        for year in range(start_year, end_year+1):
+            first_month = 1
+            last_month = 12
+            if year == start_year:
+                first_month = start_month
+            elif year == end_year:
+                last_month = end_month
+            cal.append((year,(last_month-first_month+1)))
+        cal.sort(reverse=True)
+        return cal
+
+    def join_delta(self):
+        periods = []
+        for interval_start, interval_end in self.month_periods:
+            jl_counts = self.mad.joinleave_counts(self.siteInfo.id,
+                                            self.groupInfo.id,
+                                            interval_start, interval_end)
+            leave = jl_counts.get('gs.group.member.leave', 0)
+            join = jl_counts.get('gs.group.member.join', 0)
+            periods.append((interval_start.year, interval_start.month,
+                            (join-leave)))
+        return periods_as_dict(periods)
+
+    def member_average(self):
+        periods = []
+        for interval_start, interval_end in self.month_periods:
+            result = self.mad.average_member_count(self.siteInfo.id,
+                                    self.groupInfo.id,
+                                    interval_start, interval_end)
+            
+            periods.append((interval_start.year, interval_start.month,
+                            round(result, 1)))
+        
+        return periods_as_dict(periods)
+
+    def posts(self):
+        periods = []
+        for interval_start, interval_end in self.month_periods:
+            result = self.gs.posts(self.siteInfo.id, self.groupInfo.id,
+                                              interval_start,
+                                              interval_end)
+            periods.append((interval_start.year, interval_start.month, result))
+        return periods_as_dict(periods)
+
+    def active_topics(self):
+        periods = []
+        for interval_start, interval_end in self.month_periods:
+            result = self.gs.active_topics(self.siteInfo.id, self.groupInfo.id,
+                                               interval_start,
+                                               interval_end)
+            periods.append((interval_start.year, interval_start.month, result))
+        return periods_as_dict(periods)
+
+    def authors(self):
+        periods = []
+        for interval_start, interval_end in self.month_periods:
+            result = self.gs.authors(self.siteInfo.id, self.groupInfo.id,
+                                           interval_start,
+                                           interval_end)
+            periods.append((interval_start.year, interval_start.month, result))
+        return periods_as_dict(periods)
+
+    def new_topics(self):
+        periods = []
+        for interval_start, interval_end in self.month_periods:
+            result = self.gs.new_topics(self.siteInfo.id, self.groupInfo.id,
+                                           interval_start,
+                                           interval_end)
+            periods.append((interval_start.year, interval_start.month, result))
+        return periods_as_dict(periods)
+
+    def average_dialogue_depth(self):
+        periods = []
+        for interval_start, interval_end in self.month_periods:
+            mindepth, maxdepth, avgdepth = self.gs.dialogue_depths(
+                                           self.siteInfo.id,
+                                           self.groupInfo.id,
+                                           interval_start,
+                                           interval_end)
+            periods.append((interval_start.year, interval_start.month,
+                           round(avgdepth, 1)))
+        
+        return periods_as_dict(periods)
+
+    def min_dialogue_depth(self):
+        periods = []
+        for interval_start, interval_end in self.month_periods:
+            mindepth, maxdepth, avgdepth = self.gs.dialogue_depths(
+                                           self.siteInfo.id,
+                                           self.groupInfo.id,
+                                           interval_start,
+                                           interval_end)
+            periods.append((interval_start.year, interval_start.month, mindepth))
+        return periods_as_dict(periods)
+
+    def max_dialogue_depth(self):
+        periods = []
+        for interval_start, interval_end in self.month_periods:
+            mindepth, maxdepth, avgdepth = self.gs.dialogue_depths(
+                                           self.siteInfo.id,
+                                           self.groupInfo.id,
+                                           interval_start,
+                                           interval_end)
+            periods.append((interval_start.year, interval_start.month, maxdepth))
+        return periods_as_dict(periods)
 
 class GroupStats(SitePage):
     def __init__(self, context, request):
@@ -22,11 +209,30 @@ class GroupStats(SitePage):
         retval = GroupStatsQuery(da)
         return retval
 
+    def join_delta(self):
+        mad = MembersAtDate(self.context, self.context.zsqlalchemy)
+        jl_counts = mad.joinleave_counts(self.siteInfo.id, self.groupInfo.id,
+                                    self.interval_start, self.interval_end)
+        leave = jl_counts.get('gs.group.member.leave', 0)
+        join = jl_counts.get('gs.group.member.join', 0)
+        
+        return join-leave
+
+    def member_average(self):
+        mad = MembersAtDate(self.context, self.context.zsqlalchemy)
+        return mad.average_member_count(self.siteInfo.id, self.groupInfo.id,
+                                    self.interval_start, self.interval_end)
+
+    def current_member_count(self):
+        membersInfo = GSGroupMembersInfo(self.groupInfo.groupObj)
+        return membersInfo.fullMemberCount
+
     def posts(self):
-        return self.groupStatsQuery.posts(self.siteInfo.id, self.groupInfo.id,
+        result = self.groupStatsQuery.posts(self.siteInfo.id, self.groupInfo.id,
                                           self.interval_start,
-                                          self.interval_end)     
-    
+                                          self.interval_end)
+        return result  
+
     def active_topics(self):
         return self.groupStatsQuery.active_topics(self.siteInfo.id, self.groupInfo.id,
                                            self.interval_start,
--- gs/group/stats/queries.py
+++ gs/group/stats/queries.py
@@ -1,11 +1,138 @@
 # coding=utf-8
 import sqlalchemy as sa
+import datetime
+
+from gs.group.member.log.queries import JoinLeaveQuery
+
+from gs.group.member.join.audit import JOIN_GROUP as JOIN
+from gs.group.member.join.audit import SUBSYSTEM as JOIN_SUBSYSTEM
+from gs.group.member.leave.audit import LEAVE
+from gs.group.member.leave.audit import SUBSYSTEM as LEAVE_SUBSYSTEM
+from Products.GSGroupMember.groupMembersInfo import GSGroupMembersInfo
+from zope.component import createObject
+from Products.XWFCore.XWFUtils import dt_to_user_timezone
+from Products.XWFCore.cache import simplecache
+
+from zope.cachedescriptors.property import Lazy
+
+import pytz
+
+import logging
+#logging.getLogger('sqlalchemy.engine').setLevel(logging.DEBUG)
+
+def ck_sid_gid_sp_ep(*args):
+    self, site_id, group_id, start_period, end_period = args
+    ck = ':'.join((self.da.getProperty('database'), site_id, group_id,
+                   start_period.date().isoformat(),end_period.date().isoformat()))
+    return ck
+
+def ck_sid_gid(*args):
+    self, site_id, group_id = args
+    ck = ':'.join((self.da.getProperty('database'), site_id, group_id))
+
+    return ck
+
+class MembersAtDate(JoinLeaveQuery):
+    def __init__(self, context, da):
+        self.context = context
+        self.da = da
+        JoinLeaveQuery.__init__(self, None, da)
+        self.auditEventTable = da.createTable('audit_event')
+        self.user = self.context.REQUEST.get('AUTHENTICATED_USER')
+
+        self.now = dt_to_user_timezone(context, datetime.datetime.now(pytz.utc),
+                                       self.user)
+
+    @simplecache('gs.group.stats.MembersAtDate.joinleave_counts', ck_sid_gid_sp_ep)
+    def joinleave_counts(self, site_id, group_id, start_period, end_period):
+        aet = self.auditEventTable
+        
+        start_period = dt_to_user_timezone(self.context, start_period,
+                                           self.user)
+        end_period = dt_to_user_timezone(self.context, end_period,
+                                         self.user)
+ 
+        s = sa.select([
+          aet.c.subsystem,
+          sa.func.count(aet.c.subsystem), 
+        ])
+        joinClauses = ((aet.c.subsystem == JOIN_SUBSYSTEM) & (aet.c.event_code == JOIN))
+        leaveClauses = ((aet.c.subsystem == LEAVE_SUBSYSTEM) & (aet.c.event_code == LEAVE))
+        s.append_whereclause(joinClauses | leaveClauses)
+        s.append_whereclause(aet.c.group_id == group_id)
+        s.append_whereclause(aet.c.site_id == site_id)
+        s.append_whereclause(aet.c.event_date>=start_period)
+        s.append_whereclause(aet.c.event_date<=end_period)
+        s.group_by(aet.c.subsystem)
+ 
+        r = s.execute()
+        retval = {'gs.group.member.join': 0, 'gs.group.member.leave': 0}
+        for result in r:
+            subsystem, count = result
+            assert subsystem in ('gs.group.member.join',
+                                 'gs.group.member.leave')
+            
+            retval[subsystem] = count
+
+        return retval # {'gs.group.member.join': COUNT, 'gs.group.member.leave': COUNT}
+
+    @simplecache("gs.group.stats.MembersAtDate.average_member_count", ck_sid_gid_sp_ep)
+    def average_member_count(self, site_id, group_id, start_period, end_period):
+        """ The average over a period is a rather misleading concept, since
+            it really depends on the interval at which the membership is
+            calculated. In this function it is effectively the midpoint between
+            start_period and end_period.
+
+            A rolling average taken across the days/weeks/months between
+            the start of period and now would be more accurate, but far
+            more intensive to calculate.
+
+        """
+        groupInfo = createObject('groupserver.GroupInfo', self.context, group_id)
+
+        membersInfo = GSGroupMembersInfo(groupInfo.groupObj)
+        current_member_count = membersInfo.fullMemberCount
+        
+        # first we measure the number of joins and leaves since the end_period
+        end_to_now = self.joinleave_counts(site_id, group_id, end_period, self.now)
+        
+        # then the leaves between the start and end
+        start_to_end = self.joinleave_counts(site_id, group_id, start_period, end_period)
+        endperiod_member_count = (current_member_count -
+                                  end_to_now['gs.group.member.join'] +
+                                  end_to_now['gs.group.member.leave'])
+        startperiod_member_count = (endperiod_member_count -
+                                  start_to_end['gs.group.member.join'] +
+                                  start_to_end['gs.group.member.leave'])
+
+        average_member_count = (startperiod_member_count+endperiod_member_count)/2.0
+        
+        return average_member_count
+
+    @simplecache('gs.group.stats.MembersAtDate.earliest_member_record', ck_sid_gid)
+    def earliest_member_record(self, site_id, group_id):
+        aet = self.auditEventTable
+        s = sa.select([
+          sa.func.min(aet.c.event_date),
+        ]) 
+        joinClauses = ((aet.c.subsystem == JOIN_SUBSYSTEM) & (aet.c.event_code == JOIN))
+        leaveClauses = ((aet.c.subsystem == LEAVE_SUBSYSTEM) & (aet.c.event_code == LEAVE))
+        s.append_whereclause(joinClauses | leaveClauses)
+        s.append_whereclause(aet.c.group_id == group_id)
+        s.append_whereclause(aet.c.site_id == site_id)
+
+        r = s.execute()
+        if r.rowcount:
+            return r.fetchone()[0]
+        return None
 
 class GroupStatsQuery(object):
     def __init__(self, da):
+        self.da = da
         self.topicTable = da.createTable('topic')
         self.postTable  = da.createTable('post')
-    
+ 
+    @simplecache('gs.group.stats.GroupStatsQuery.posts', ck_sid_gid_sp_ep)
     def posts(self, site_id, group_id, start_period, end_period):
         t = self.postTable 
         s = t.select()
@@ -21,20 +148,46 @@ class GroupStatsQuery(object):
         
         return retval
 
+    @simplecache('gs.group.stats.GroupStatsQuery.active_topics', ck_sid_gid_sp_ep)
     def active_topics(self, site_id, group_id, start_period, end_period):
-        t = self.topicTable
-        s = t.select()
-
-        s.append_whereclause(t.c.site_id==site_id)
-        s.append_whereclause(t.c.group_id==group_id)
-        s.append_whereclause(t.c.last_post_date>=start_period)
-        s.append_whereclause(t.c.last_post_date<=end_period)
+        p = self.postTable
+        s = sa.select([p.c.topic_id], distinct=True, group_by=p.c.topic_id)
+        s.append_whereclause(p.c.site_id==site_id)
+        s.append_whereclause(p.c.group_id==group_id)
+        s.append_whereclause(p.c.date>=start_period)
+        s.append_whereclause(p.c.date<=end_period)
         
         r = s.execute()
         retval = r.rowcount
 
         return retval
 
+    @simplecache('gs.group.stats.GroupStatsQuery.dialogue_depths', ck_sid_gid_sp_ep)
+    def dialogue_depths(self, site_id, group_id, start_period, end_period):
+        p = self.postTable
+        s = sa.select([sa.func.count(p.c.topic_id)], group_by=p.c.topic_id)
+        s.append_whereclause(p.c.site_id==site_id)
+        s.append_whereclause(p.c.group_id==group_id)
+        s.append_whereclause(p.c.date>=start_period)
+        s.append_whereclause(p.c.date<=end_period)
+        
+        r = s.execute()
+        count = r.rowcount
+        depths = []
+        if count:
+            for row in r:
+                depths.append(row[0])
+
+        total = sum(depths)
+        avgdepth = 0; mindepth = 0; maxdepth = 0
+        if count:
+            avgdepth = float(total)/float(count) 
+            mindepth = min(depths)
+            maxdepth = max(depths)    
+ 
+        return mindepth, maxdepth, avgdepth
+
+    @simplecache('gs.group.stats.GroupStatsQuery.new_topics', ck_sid_gid_sp_ep)
     def new_topics(self, site_id, group_id, start_period, end_period):
         t = self.postTable
 
@@ -52,6 +205,7 @@ class GroupStatsQuery(object):
 
         return retval
 
+    @simplecache('gs.group.stats.GroupStatsQuery.authors', ck_sid_gid_sp_ep)
     def authors(self, site_id, group_id, start_period, end_period):
         t = self.postTable
         s = sa.select([t.c.user_id], distinct=True)
