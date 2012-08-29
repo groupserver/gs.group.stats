1b555d79b1b4471b847b255456fc208b01c6902b
richard@onlinegroups.net <richard@onlinegroups.net>
2012-08-29 17:05:11 +1200

    Add posts via web, refactor into ContentProvider

--- gs/group/stats/page.py
+++ gs/group/stats/page.py
@@ -1,5 +1,13 @@
-from Products.Five import BrowserView
 from gs.content.base.page import SitePage
+from zope.component import createObject
+from zope.cachedescriptors.property import Lazy
 
 class GroupStatsView(SitePage):
-    pass
+    def __init__(self, context, request):
+        SitePage.__init__(self, context, request)
+
+    @Lazy
+    def groupInfo(self):
+        retval = createObject('groupserver.GroupInfo', self.context)
+        assert retval, 'Could not create the GroupInfo from %s' % self.context
+        return retval
