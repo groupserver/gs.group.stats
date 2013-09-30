8f62c33f833647291f80c6f965ddf3c11327e396
Michael JasonSmith <mpj17@onlinegroups.net>
2013-09-30 16:09:09 -0700

    Adding a TODO

--- gs/group/stats/__init__.py
+++ gs/group/stats/__init__.py
@@ -1,6 +1,6 @@
+# -*- coding: utf-8 -*-
+# TODO: Figure out why the following is HERE
 from AccessControl import ModuleSecurityInfo
-from AccessControl import allow_class
 
 utils_security = ModuleSecurityInfo('gs.skin.ogn.edem.utils')
 utils_security.declarePublic('fn_to_nickname')
-
