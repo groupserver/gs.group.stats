19f49a366b0575826f27ec68268b5d20bcbe419e
Michael JasonSmith <mpj17@onlinegroups.net>
2013-10-02 10:09:07 -0700

    Python 3 prep and adding a Copyright block.

--- gs/group/stats/messagequery.py
+++ gs/group/stats/messagequery.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 from datetime import date
 import sqlalchemy as sa
 from gs.database import getSession
@@ -39,7 +52,7 @@ class MessageQuery(MLMQ):
 
         years = {}
         for row in rows:
-            if row['year'] not in years.keys():
+            if row['year'] not in years:
                 years[row['year']] = {}
         for row in rows:
             years[row['year']][row['month']] = {'post_count': row['post_count']}
