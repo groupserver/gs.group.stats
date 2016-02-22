6139b6c9cf1ae5bb77db87897b576581a355e39a
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-22 09:46:11 -0800

    Using the new FullMembers list.

--- gs/group/stats/contentprovider.py
+++ gs/group/stats/contentprovider.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Copyright © 2013 OnlineGroups.net and Contributors.
+# Copyright © 2013, 2016 OnlineGroups.net and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -12,13 +12,13 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals, print_function
 from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
 from zope.contentprovider.interfaces import UpdateNotCalled
 from zope.pagetemplate.pagetemplatefile import PageTemplateFile
+from gs.group.member.base import FullMembers
 from gs.viewlet import SiteContentProvider  # --=mpj17=-- Deliberly *site*
-from Products.GSGroupMember.groupMembersInfo import GSGroupMembersInfo
 from .posting import GroupPostingStats
 
 
@@ -32,8 +32,7 @@ class GroupStatsContentProvider(SiteContentProvider):
         self.__updated = True
         self.groupPostingStats = GroupPostingStats(self.groupInfo)
         self.groupPostingStats.update()
-
-        self.groupMembersInfo = GSGroupMembersInfo(self.groupInfo.groupObj)
+        self.fullMembers = FullMembers(self.groupInfo.groupObj)
 
     def render(self):
         if not self.__updated:
@@ -52,8 +51,7 @@ class GroupStatsContentProvider(SiteContentProvider):
         #   can be passed the identifier for the group, and it uses
         #   that to find the group and render the right statistics.
         if self.groupId:
-            retval = createObject('groupserver.GroupInfo', self.context,
-                                    self.groupId)
+            retval = createObject('groupserver.GroupInfo', self.context, self.groupId)
         else:
             # We have no groupId passed in from the surrounding page
             #   template, so lets assume that we are in the context
